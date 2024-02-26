5b58c6287c1c874eeffa41153b78faaba14bbc5a
marcovittoriozacca <zaccamarco01@gmail.com>
2024-02-26 13:44:26 +0100

    feat: values are now in a table

--- index.php
+++ index.php
@@ -39,6 +39,7 @@
         ],
 
     ];
+    $keys = array_keys($hotels[0]);
 
 ?>
 
@@ -47,32 +48,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Hotel</title>
 </head>
 <body>
     
-    <header>
+    <header class="text-center py-2">
         <h1>HOTEL</h1>
     </header>
 
     <main>
-        <?php foreach($hotels as $hotel): ?>
-        <ul>
-            <li>
-                <?= $hotel['name'] ?>
-                <br>
-                <?= $hotel['description'] ?>
-                <br>
-                <?= ($hotel['parking'] == 1) ? 'true' : 'false'; ?>
-                <br>
-                <?= $hotel['vote'] ?>
-                <br>
-                <?= $hotel['distance_to_center'] ?> Km
-
-            </li>
-        </ul>
-        <?php endforeach;?>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr class="text-center">
+                            <?php foreach($keys as $key): ?>
+                                <th class="text-capitalize"> <?= $key ?> </th>
+                            <?php endforeach;?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($hotels as $hotel): ?>
+                                <tr class="text-center">
+                                <?php foreach($hotel as $element): ?>
+                                    <td> <?= $element ?> </td>
+                                <?php endforeach;?>
+                                </tr>
+                            <?php endforeach;?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </main>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
